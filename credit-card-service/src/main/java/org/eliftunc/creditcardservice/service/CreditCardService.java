244a1490085be981e84a227c2e19f40c3c88a895
eliftuncc21 <100561767+eliftuncc21@users.noreply.github.com>
2025-06-15 01:34:39 +0300

    Credit Card Service, Caching implemented

--- credit-card-service/src/main/java/org/eliftunc/creditcardservice/service/CreditCardService.java
+++ credit-card-service/src/main/java/org/eliftunc/creditcardservice/service/CreditCardService.java
@@ -7,6 +7,11 @@ import org.eliftunc.creditcardservice.entity.CreditCard;
 import org.eliftunc.creditcardservice.mapper.CreditCardMapper;
 import org.eliftunc.creditcardservice.repository.CreditCardProjection;
 import org.eliftunc.creditcardservice.repository.CreditCardRepository;
+import org.eliftunc.creditcardservice.utils.Caches;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +23,7 @@ public class CreditCardService {
     private final CreditCardRepository creditCardRepository;
     private final CreditCardMapper creditCardMapper;
 
+    @CacheEvict(value = Caches.CARDS_CACHE, allEntries = true)
     public CreditCardResponseDto createCreditCard(CreditCardRequestDto creditCardRequestDto) {
         CreditCard card = creditCardMapper.toCreditCard(creditCardRequestDto);
         creditCardRepository.save(card);
@@ -25,18 +31,22 @@ public class CreditCardService {
         return creditCardMapper.toCreditCardResponseDto(card);
     }
 
+    @Cacheable(value = Caches.CARDS_CACHE, key = "'all'")
     public Page<CreditCardResponseDto> getCreditCard(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         return creditCardRepository.findAll(pageable)
                 .map(creditCardMapper::toCreditCardResponseDto);
     }
 
+    @Cacheable(value = Caches.CARD_CACHE, key = "#cardId")
     public CreditCardResponseDto getCreditCardById(Long cardId){
         return creditCardRepository.findById(cardId)
                 .map(creditCardMapper::toCreditCardResponseDto)
                 .orElse(null);
     }
 
+    @CachePut(value = Caches.CARD_CACHE, key = "#cardId")
+    @CacheEvict(value = Caches.CARDS_CACHE, allEntries = true)
     public CreditCardResponseDto updateCreditCard(CreditCardRequestDto creditCardRequestDto, Long cardId) {
         CreditCard card = creditCardRepository.findById(cardId).orElse(null);
         creditCardMapper.updateCreditCard(creditCardRequestDto, card);
@@ -44,6 +54,12 @@ public class CreditCardService {
         return creditCardMapper.toCreditCardResponseDto(card);
     }
 
+    @Caching(
+            evict = {
+                    @CacheEvict(value = Caches.CARDS_CACHE, allEntries = true),
+                    @CacheEvict(value = Caches.CARD_CACHE, key = "#id")
+            }
+    )
     public void deleteCreditCardById(Long cardId) {
         if (creditCardRepository.existsById(cardId)){
             throw new RuntimeException("Credit card not found");
@@ -51,6 +67,7 @@ public class CreditCardService {
         creditCardRepository.deleteById(cardId);
     }
 
+    @Cacheable(value = Caches.CARD_CACHE, key = "'user_' + #userId")
     public CreditCardProjection getCreditCArdByUserId(Long userId){
         return creditCardRepository.findProjectionByUserId(userId);
     }
---
+++ credit-card-service/src/main/java/org/eliftunc/creditcardservice/utils/Caches.java
@@ -0,0 +1,6 @@
+package org.eliftunc.creditcardservice.utils;
+
+public class Caches {
+    public static final String CARD_CACHE = "card-cache";
+    public static final String CARDS_CACHE = "cards-cache";
+}
