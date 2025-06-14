6196062d41ba14cf2e02425e7970b26bb7370207
eliftuncc21 <100561767+eliftuncc21@users.noreply.github.com>
2025-06-14 19:27:03 +0300

    Implemented REST API endpoints for user-related POST, GET, PUT, and DELETE requests

---
+++ user-service/src/main/java/org/eliftunc/userservice/controller/UserController.java
@@ -0,0 +1,52 @@
+package org.eliftunc.userservice.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.eliftunc.userservice.dto.UserRequestDto;
+import org.eliftunc.userservice.dto.UserResponseDto;
+import org.eliftunc.userservice.repository.UserProjection;
+import org.eliftunc.userservice.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/create-user")
+    public UserResponseDto createUser(@RequestBody @Valid UserRequestDto userRequestDto) {
+        return userService.createUser(userRequestDto);
+    }
+
+    @GetMapping("/email/{email}")
+    public UserResponseDto getUserByEmail(@PathVariable String email) {
+        return userService.findByEmail(email);
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/list-user/{page}/{size}")
+    public Page<UserResponseDto> getUsers(@PathVariable int page, @PathVariable int size) {
+        return userService.getAllUsers(page, size);
+    }
+
+    @PutMapping("/update-user/{id}")
+    public UserResponseDto updateUser(@RequestBody @Valid UserRequestDto userRequestDto, @PathVariable Long id) {
+        return userService.updateUser(userRequestDto, id);
+    }
+
+    @DeleteMapping("/delete-user/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+    @GetMapping("/user/{id}")
+    public UserProjection findUserById(@PathVariable Long id) {
+        return userService.findByUser(id);
+    }
+}
