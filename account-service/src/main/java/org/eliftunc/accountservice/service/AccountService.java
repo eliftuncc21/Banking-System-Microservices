8eff0a8c5d4947186efd9f19e7cc5521982a588c
eliftuncc21 <100561767+eliftuncc21@users.noreply.github.com>
2025-06-15 00:17:59 +0300

    Account service, Caching implemented

--- account-service/src/main/java/org/eliftunc/accountservice/service/AccountService.java
+++ account-service/src/main/java/org/eliftunc/accountservice/service/AccountService.java
@@ -7,6 +7,11 @@ import org.eliftunc.accountservice.entity.Account;
 import org.eliftunc.accountservice.mapper.AccountMapper;
 import org.eliftunc.accountservice.repository.AccountProjection;
 import org.eliftunc.accountservice.repository.AccountRepository;
+import org.eliftunc.accountservice.utils.Caches;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +23,7 @@ public class AccountService {
     private final AccountRepository accountRepository;
     private final AccountMapper accountMapper;
 
+    @CacheEvict(value = Caches.ACCOUNTS_CACHE, allEntries = true)
     public AccountResponseDto createAccount(AccountRequestDto accountRequestDto){
         Account account = accountMapper.toAccount(accountRequestDto);
         accountRepository.save(account);
@@ -25,18 +31,22 @@ public class AccountService {
         return accountMapper.toAccountResponseDto(account);
     }
 
+    @Cacheable(value = Caches.ACCOUNTS_CACHE, key = "'all'")
     public Page<AccountResponseDto> getAccount(int page, int size){
         Pageable pageable = PageRequest.of(page, size);
         return accountRepository.findAll(pageable)
                 .map(accountMapper::toAccountResponseDto);
     }
 
+    @Cacheable(value = Caches.ACCOUNT_CACHE, key = "#accountId")
     public AccountResponseDto getAccountById(Long accountId){
         return accountRepository.findById(accountId)
                 .map(accountMapper::toAccountResponseDto)
                 .orElse(null);
     }
 
+    @CachePut(value = Caches.ACCOUNT_CACHE, key = "#id")
+    @CacheEvict(value = Caches.ACCOUNTS_CACHE, allEntries = true)
     public AccountResponseDto updateAccount(Long id, AccountRequestDto accountRequestDto){
         Account account = accountRepository.findById(id).orElse(null);
         accountMapper.updateAccount(accountRequestDto, account);
@@ -44,6 +54,12 @@ public class AccountService {
         return accountMapper.toAccountResponseDto(account);
     }
 
+    @Caching(
+            evict = {
+                    @CacheEvict(value = Caches.ACCOUNT_CACHE, key = "#id"),
+                    @CacheEvict(value = Caches.ACCOUNTS_CACHE, allEntries = true)
+            }
+    )
     public void deleteAccount(Long id){
         if(!accountRepository.existsById(id)){
             throw new RuntimeException("Account not found");
@@ -51,6 +67,7 @@ public class AccountService {
         accountRepository.deleteById(id);
     }
 
+    @Cacheable(value = Caches.ACCOUNT_CACHE, key= "'user_' + #userId")
     public AccountProjection getAccountByUserId(Long userId){
         return accountRepository.findProjectionByUserId(userId);
     }
---
+++ account-service/src/main/java/org/eliftunc/accountservice/utils/Caches.java
@@ -0,0 +1,6 @@
+package org.eliftunc.accountservice.utils;
+
+public class Caches {
+    public static final String ACCOUNT_CACHE = "account-cache";
+    public static final String ACCOUNTS_CACHE = "accounts-cache";
+}
