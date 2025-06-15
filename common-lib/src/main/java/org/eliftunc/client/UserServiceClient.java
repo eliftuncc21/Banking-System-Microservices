33e06b2a2cfb29d06f5ca47965f280868059cc9a
eliftuncc21 <100561767+eliftuncc21@users.noreply.github.com>
2025-06-15 13:15:36 +0300

    UserServiceClient was added to common-lib for communication via Feign.

---
+++ common-lib/src/main/java/org/eliftunc/client/UserServiceClient.java
@@ -0,0 +1,12 @@
+package org.eliftunc.client;
+
+import org.eliftunc.dto.UserResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-service", url = "http://localhost:8081")
+public interface UserServiceClient {
+    @GetMapping("/api/users/{id}")
+    UserResponseDto getUserById(@PathVariable Long id);
+}
