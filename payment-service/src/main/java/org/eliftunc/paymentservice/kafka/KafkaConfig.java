e582a9348266f3df1dd25d9f28447a6b412c51c1
eliftuncc21 <100561767+eliftuncc21@users.noreply.github.com>
2025-06-16 00:10:35 +0300

    Kafka configuration added to payment service

---
+++ payment-service/src/main/java/org/eliftunc/paymentservice/kafka/KafkaConfig.java
@@ -0,0 +1,23 @@
+package org.eliftunc.paymentservice.kafka;
+
+import org.eliftunc.events.PaymentCreatedEvent;
+import org.eliftunc.kafka.KafkaProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaConfig {
+    private final KafkaProducerConfig kafkaProducerConfig = new KafkaProducerConfig();
+
+    @Bean
+    public ProducerFactory<String, PaymentCreatedEvent> consumerFactory() {
+        return kafkaProducerConfig.kafkaProducer();
+    }
+
+    @Bean
+    public KafkaTemplate<String, PaymentCreatedEvent> kafkaTemplate(){
+        return kafkaProducerConfig.kafkaTemplate();
+    }
+}
