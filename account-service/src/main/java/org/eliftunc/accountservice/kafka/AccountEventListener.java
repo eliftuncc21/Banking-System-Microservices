983ab67b939df23a00f7836ee2d5b4da85e8700c
eliftuncc21 <100561767+eliftuncc21@users.noreply.github.com>
2025-06-16 01:07:56 +0300

    Add Kafka listener for PaymentCreatedEvent in AccountEventListener

--- account-service/src/main/java/org/eliftunc/accountservice/kafka/AccountEventListener.java
+++ account-service/src/main/java/org/eliftunc/accountservice/kafka/AccountEventListener.java
@@ -3,6 +3,8 @@ package org.eliftunc.accountservice.kafka;
 import lombok.RequiredArgsConstructor;
 import org.eliftunc.accountservice.entity.Account;
 import org.eliftunc.accountservice.repository.AccountRepository;
+import org.eliftunc.enums.PaymentType;
+import org.eliftunc.events.PaymentCreatedEvent;
 import org.eliftunc.events.TransactionCreatedEvent;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -21,5 +23,19 @@ public class AccountEventListener {
         toAccount.setBalance(toAccount.getBalance() + transactionEvent.getAmount());
         accountRepository.save(fromAccount);
         accountRepository.save(toAccount);
+    }
+
+    @KafkaListener(topics = "payment-service", groupId = "account-service-group",
+            containerFactory = "paymentKafkaListenerContainerFactory")
+    public void paymentConsumerAccountCreated(PaymentCreatedEvent paymentCreatedEvent) {
+
+        if(paymentCreatedEvent.getPaymentType() != PaymentType.BANK_CARD){
+            throw new RuntimeException("Only credit card payments are supported.");
+        }
+
+        Account account = accountRepository.findAccountByUserId(paymentCreatedEvent.getUserId());
+
+        account.setBalance(account.getBalance() - paymentCreatedEvent.getAmount());
+        accountRepository.save(account);
     }
 }
